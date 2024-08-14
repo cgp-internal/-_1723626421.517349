7c5bc41b4741e34529128c4409ecdc89329b8dd9
cgp-client <code.gen.pro.system@gmail.com>
2024-08-14 12:07:15 +0300

    Adding src/components/App.js

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+export function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main>
+        {/* Add your application content here */}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
